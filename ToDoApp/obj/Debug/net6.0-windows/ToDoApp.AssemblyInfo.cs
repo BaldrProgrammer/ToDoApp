9578232f4842793722cfb6bbd5bef35916184fda
4.12 <netu@fd.puk>
2025-11-05 10:16:51 +0100

    +cmbFilter widg

--- ToDoApp/obj/Debug/net6.0-windows/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net6.0-windows/ToDoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271ee7d24e5af8ca7bbb986bc148b51db4e7482e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
